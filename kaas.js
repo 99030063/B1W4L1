5e69bf80d80184c748fb29650ef6303c7d37332e
Wesley <99030063@mydavinci.nl>
2019-09-25 14:24:00 +0200

    added refresh button

--- kaas.js
+++ kaas.js
@@ -7,21 +7,22 @@ function answerQuestion(questionNo, answer){
     if(answer==true){
         if(vragen[vragen[questionNo]['nextIfYes']]['cheesetype']==null){
             vraag.innerHTML = vragen[vragen[questionNo]['nextIfYes']]['vraag']
-            jaKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfYes'] +', true)')
-            neeKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfYes'] + ', false)')
+            jaKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfYes'] +', true)');
+            neeKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfYes'] + ', false)');
         }else{
             document.getElementById("antwoord").innerHTML = "U bent waarschijnlijk op zoek naar " + "<strong>"+vragen[vragen[questionNo]['nextIfYes']]['cheesetype']+"</strong>";
             document.getElementById("response").remove();
+            document.getElementById("buttonAppear").innerHTML = '<button type=button onclick="window.location.reload()">Herstarten</button>';
         }
     }else{
         if(vragen[vragen[questionNo]['nextIfNo']]['cheesetype']==null){
             vraag.innerHTML = vragen[vragen[questionNo]['nextIfNo']]['vraag']
-            jaKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfNo'] +', true)')
-            neeKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfNo'] + ', false)')
+            jaKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfNo'] +', true)');
+            neeKnop.setAttribute('onclick','answerQuestion('+ vragen[questionNo]['nextIfNo'] + ', false)');
         }else{
             document.getElementById("antwoord").innerHTML = "U bent waarschijnlijk op zoek naar " + "<strong>"+vragen[vragen[questionNo]['nextIfNo']]['cheesetype']+"</strong>";
             document.getElementById("response").remove();
-           
+            document.getElementById("buttonAppear").innerHTML = '<button type=button onclick="window.location.reload()">Herstarten</button>';  
         }
     } 
 
